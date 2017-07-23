8120d1ebed60580806956ade37291ce91f93caa2
reckful <1970709700@qq.com>
2017-07-23 16:01:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app"> 
+    <img src="./assets/logo.png" alt="">
+    <span>Vue Markdown</span>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+import editor from './components/editor'
+export default {
+  name: 'app',
+  components: {
+    editor 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+#app img{
+  height: 100px;
+  width:100px;
+}
+#app span{
+  font-size: 35px;
+  position: relative;
+  top:-30px;
+  font-weight:bold;
+}
+</style>
